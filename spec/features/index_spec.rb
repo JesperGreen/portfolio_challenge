e798162b4cdc0b7bfd728c96b6af74e95914581b
Jesper <jesper_gs_@hotmail.com>
2016-08-30 10:37:46 +0200

    added data/projects.yml and made it pass in rspec

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -16,4 +16,12 @@ describe 'index', type: :feature do
   it 'displays Middleman logo' do
     expect(page).to have_css 'img[src*="middleman-logo.svg"]'
   end
+
+  it 'displays project list' do
+    expect(page).to have_css '.projects'
+    within '.projects' do
+      expect(page).to have_content 'My First Website'
+      expect(page).to have_content 'FizzBuzz'
+    end
+  end
 end
